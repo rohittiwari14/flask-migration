8f4b958075d6e9804246f5257fca50357cdfa49c
sampada <tiwarir928@gmail.com>
2024-09-05 15:58:15 +0530

    Added JWT Functionallity, Regular Expressions and User can sign-in either using a mobile number or an email

--- config.py
+++ config.py
@@ -4,3 +4,6 @@ class Config:
     debug = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///mydatabase.db'
     SQLALCHEMY_TRACK_MODIFICATION_ = False
+    SECRET_KEY = "rohittiwari"
+    EMAIL_REGEX = r"^[^\s@]+@[^\s@]+\.[^\s@]+$"
+    PHONE_REGEX = r"^\d{10}$"
--- routes.py
+++ routes.py
@@ -1,7 +1,11 @@
 from flask import request, Blueprint, jsonify
 from werkzeug.security import check_password_hash, generate_password_hash
 from extensions import db
+from config import Config
 from models import User
+import datetime
+import jwt
+import re
 
 routes_bp = Blueprint("routes", __name__)
 
@@ -10,17 +14,31 @@ routes_bp = Blueprint("routes", __name__)
 def sign_in():
     try:
         if request.method == "POST":
-            email = request.form.get('email')
+            user_id = request.form.get('email') or request.form.get('mobile_no')
             password = request.form.get('password')
-            if not email or not password:
+            if not user_id or not password:
                 return jsonify({"error": "Email and Password are required"}), 400
-            user = User.query.filter_by(email=email).first()
-            if user is None:
-                return jsonify({"error": "Invalid email ID."}), 400
-            elif not check_password_hash(user.password, password):
-                return jsonify({"error": "Incorrect Password"}), 400
-            else:
-                return jsonify({"message": "Sign-In Successful."}), 200
+            if re.match(Config.EMAIL_REGEX, user_id):
+                user = User.query.filter_by(email=user_id).first()
+                if check_password_hash(user.password, password):
+                    token = jwt.encode({
+                        'user_id': user.id,
+                        'exp': datetime.datetime.now() + datetime.timedelta(hours=1)
+                    }, Config.SECRET_KEY)
+                    return jsonify({"token": token, "message": "Sign-In Successful."}), 200
+                else:
+                    return jsonify({"error": "Invalid Password."}), 400
+            elif re.match(Config.PHONE_REGEX, user_id):
+                user = User.query.filter_by(mobile_no=user_id).first()
+                if check_password_hash(user.password, password):
+                    token = jwt.encode({
+                        'user_id': user.id,
+                        'exp': datetime.datetime.now() + datetime.timedelta(hours=1)
+                    }, Config.SECRET_KEY)
+                    return jsonify({"token": token, "message": "Sign-In Successful."}), 200
+                else:
+                    return jsonify({"error": "Invalid Password."}), 400
+                
         return jsonify({"message": "Welcome to Sign-In."})
 
     except Exception as e:
@@ -31,10 +49,16 @@ def sign_in():
 def sign_up():
     if request.method == "POST":
         try:
+            mobile = request.form.get('mobile')
+            if not re.match(Config.PHONE_REGEX, mobile):
+                return jsonify({'error': 'The Mobile Number must be 10 digits.'})
+            email = request.form.get('email')
+            if not re.match(Config.EMAIL_REGEX, email):
+                return jsonify({'error': "Email must contain '@' and '.com'."})
             new_user = User(
                 name=request.form.get('name'),
-                mobile_no=request.form.get('mobile'),
-                email=request.form.get('email'),
+                mobile_no=mobile,
+                email=email,
                 password=generate_password_hash(request.form.get('password'), method='pbkdf2:sha256', salt_length=8),
                 age=request.form.get('age'),
                 address=request.form.get('address')
